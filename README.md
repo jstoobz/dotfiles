a33c90e879ab6a4e662f5f9ae0d572540e90ef12
James Stephens <stephensja7@gmail.com>
2021-02-07 23:17:51 -0500

    added xcode cli tools and homebrew to bootstrap

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # dotfiles
+
+## Setup
+
+Install dotfiles by running the following in a terminal:
+
+```shell
+/bin/bash -c "$(curl -LsS https://raw.githubusercontent.com/jstoobz/dotfiles/main/bootstrap.sh)"
+```
---
+++ bootstrap.sh
@@ -0,0 +1,141 @@
+#!/bin/sh
+
+set -e
+
+DOTFILES_ROOT=$(pwd -P)
+
+GITHUB_REPOSITORY="jstoobz/dotfiles"
+DOTFILES_TARBALL_URL="https://github.com/$GITHUB_REPOSITORY/tarball/main"
+DOTFILES_HOME_DIR="${HOME}/.dotfiles"
+
+OSX_VERS=$(sw_vers -productVersion)
+SW_BUILD=$(sw_vers -buildVersion)
+
+# Color Utilities
+_RESET=$(tput sgr0)
+_GREEN_BOLD=$(
+  tput setaf 2
+  tput bold
+)
+
+banner() {
+  cat <<EOF
+${_GREEN_BOLD}
+       _     _              _
+      (_)___| |_ ___   ___ | |__ ____
+      | / __| __/ _ \ / _ \| '_ \_  /
+      | \__ \ || (_) | (_) | |_) / /
+     _/ |___/\__\___/ \___/|_.__/___|
+    |__/
+	By James Stephens (jstoobz)
+${_RESET}	
+EOF
+}
+
+info() {
+  # shellcheck disable=SC2059
+  printf "\r  [ \033[00;34m..\033[0m ] $1\n"
+}
+
+user() {
+  # shellcheck disable=SC2059
+  printf "\r  [ \033[0;33m??\033[0m ] $1\n"
+}
+
+success() {
+  # shellcheck disable=SC2059
+  printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
+}
+
+fail() {
+  # shellcheck disable=SC2059
+  printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n\n"
+  # exit
+}
+
+clr_screen() {
+  # shellcheck disable=SC2059
+  printf "\033c"
+}
+
+padding() {
+  # shellcheck disable=SC2059
+  printf "\n"
+}
+
+ask_for_sudo() {
+  # Ask for the administrator password upfront
+  sudo -v
+
+  # Update existing `sudo` time stamp until the script has finished
+  while true; do
+    sudo -n true
+    sleep 60
+    kill -0 "$$" || exit
+  done 2>/dev/null &
+}
+
+command_exists() {
+  command -v "$@" >/dev/null 2>&1
+}
+
+extract() {
+  archive="$1"
+  outputDir="$2"
+
+  command_exists "tar" &&
+    tar -zxf "$archive" --strip-components 1 -C "$outputDir"
+}
+
+install_xcode_cli_tools() {
+  info "Checking for Xcode Command Line Tools..."
+
+  if [ -f "/Library/Developer/CommandLineTools/usr/bin/git" ]; then
+    success "Xcode Command Line Tools already installed"
+    return
+  fi
+
+  info "Installing the Xcode Command Line Tools:"
+
+  # This temporary file prompts the 'softwareupdate' utility to list the Command Line Tools
+  CLT_PLACEHOLDER="/tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress"
+  touch "${CLT_PLACEHOLDER}"
+  CLT_PACKAGE=$(softwareupdate -l |
+    grep -B 1 "Command Line Tools" |
+    awk -F"*" '/^ *\*/ {print $2}' |
+    sed -e 's/^ *Label: //' -e 's/^ *//' |
+    sort -V |
+    tail -n1)
+
+  softwareupdate -i "${CLT_PACKAGE}"
+  rm -rf "${CLT_PLACEHOLDER}"
+  success "Installed Xcode"
+}
+
+install_homebrew() {
+  info "Checking for Homebrew..."
+
+  if ! command_exists brew; then
+    info "Installing Homebrew..."
+    printf "\n" |
+      /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+    success "Installed Homebrew"
+  fi
+
+	brew update
+  brew upgrade
+  success "Updated Homebrew"
+}
+
+main() {
+  ask_for_sudo "$@"
+  clr_screen "$@"
+  padding "$@"
+  banner "$@"
+  info "MacOS Version: ${OSX_VERS}"
+  info "MacOS SW Build: ${SW_BUILD}"
+  install_xcode_cli_tools "$@"
+  install_homebrew "$@"
+}
+
+main "$@"
